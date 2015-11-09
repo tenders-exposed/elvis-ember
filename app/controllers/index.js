4b066cea42180dc90ea87be07daf8f5a54fa426d
Victor Nițu <victor@thesponge.eu>
2015-11-09 23:05:53 +0200

    Fighting bugs, git conflicts suck

---
+++ app/controllers/index.js
@@ -0,0 +1,34 @@
+import Ember from 'ember';
+
+export default Ember.Controller.extend({
+  user: Ember.computed(function() {
+    return this.store.createRecord('user');
+  }),
+
+  actions: {
+    register: function() {
+      var self = this;
+      this.get('user').save().then(function() {
+        self.notifications.addNotification({
+          message: 'Done! Please check your inbox.',
+          type: 'success'
+        });
+        self.transitionToRoute('index');
+      }, function(response) {
+          console.error('There was a problem', response);
+          Object.keys(response.errors).map(function(value, index) {
+            console.log(index);
+            response.errors.each(function(){
+              self.notifications.addNotification({
+                message: this[0],
+                type: 'error',
+                autoClear: true,
+                clearDuration: 2500
+              });
+            });
+          });
+        }
+      );
+    },
+  }
+});
