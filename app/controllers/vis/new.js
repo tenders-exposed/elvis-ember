1598badbe6dc8e0de85c37c33e2cac635e725bc0
Victor Nițu <victor@thesponge.eu>
2015-12-17 23:33:25 +0200

    Upgrade to Ember 2.2.x, reorganized structure

---
+++ app/controllers/vis/new.js
@@ -0,0 +1,40 @@
+import Ember from 'ember';
+
+export default Ember.Controller.extend({
+  country_select2: [
+    {
+      id: 1,
+      text: 'one',
+      description: 'one description'
+    },
+    {
+      id: 2,
+      text: 'two',
+      description: 'one description'
+    },
+    {
+      id: 3,
+      text: 'three',
+      description: 'one description'
+    },
+  ],
+  country_select: [
+    {
+      name: 'one',
+      description: 'one description'
+    },
+    {
+      name: 'two',
+      description: 'one description'
+    },
+    {
+      name: 'three',
+      description: 'one description'
+    },
+  ],
+  actions: {
+    onSelectEvent: function(value){
+      Ember.debug( "New select value: %@".fmt( value ) );
+    }
+  }
+});
--- app/routes/vis/new.js
+++ app/routes/vis/new.js
@@ -4,4 +4,9 @@ export default Ember.Route.extend({
 //  renderTemplate: function(){
 //    this.render('graph/new');
 //  }
+  actions: {
+    changedAction: function(value) {
+      Ember.debug( "New slider value: %@".fmt( value ) );
+    }
+  }
 });
