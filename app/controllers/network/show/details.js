eea7ac6050ed67f878e1f93d75ca0b32d8cb6bd1
Victor Nițu <victor@thesponge.eu>
2015-12-30 22:03:56 +0200

    Ember suave compliance

--- app/controllers/network/show/details.js
+++ app/controllers/network/show/details.js
@@ -2,14 +2,14 @@ import Ember from 'ember';
 
 export default Ember.Controller.extend({
   sidebarTabs: [
-    {id: 'suppliers', title: 'Suppliers'},
-    {id: 'procurers', title: 'Procurers'},
-    {id: 'relationships', title: 'Relationshits'},
+    { id: 'suppliers', title: 'Suppliers' },
+    { id: 'procurers', title: 'Procurers' },
+    { id: 'relationships', title: 'Relationshits' }
   ],
   active: 'suppliers',
   actions: {
     changeTab(tab) {
-      this.set("active", tab);
+      this.set('active', tab);
     }
   }
 });
--- app/initializers/authentication.js
+++ app/initializers/authentication.js
@@ -1,22 +1,22 @@
-//export function initialize(/* container, application */) {
-//  // application.inject('route', 'foo', 'service:foo');
-//}
+// export function initialize(/* container, application */) {
+//   // application.inject('route', 'foo', 'service:foo');
+// }
 //
-//export default {
-//  name: 'authentication',
-//  initialize: initialize
-//};
+// export default {
+//   name: 'authentication',
+//   initialize: initialize
+// };
 export default {
   name:       'authentication',
   /*   after:      'simple-auth', */
-  //initialize: function(container) {
-  initialize: function() {
-  //  /*   var applicationRoute = container.lookup('route:application');
-  //     var session          = container.lookup('simple-auth-session:main');
-  //     // handle the session events?
-  //     session.on('sessionAuthenticationSucceeded', function() {
-  //     applicationRoute.transitionTo('match.dashboard');
-  //     });
-  //  */
+  // initialize: function(container) {
+  initialize() {
+    //  /*   var applicationRoute = container.lookup('route:application');
+    //     var session          = container.lookup('simple-auth-session:main');
+    //     // handle the session events?
+    //     session.on('sessionAuthenticationSucceeded', function() {
+    //     applicationRoute.transitionTo('match.dashboard');
+    //     });
+    //  */
   }
 };
