735ebb1593026d43d2ac8576ff363d18fcc270bf
Victor Nițu <victor@thesponge.eu>
2016-03-20 08:30:42 +0200

    Try to get Mongo ID as a sane property

--- app/models/network.js
+++ app/models/network.js
@@ -3,10 +3,14 @@ import DS from 'ember-data';
 export default DS.Model.extend({
   // nodes: DS.attr(),
   // edges: DS.attr(),
+  _id: DS.attr(),
   query: DS.attr(),
   options: DS.attr(),
   name: DS.attr('string'),
   description: DS.attr('string'),
-  graph: DS.attr()
+  graph: DS.attr(),
 
+  //id: Ember.computed('_id', function(){
+  //  return this.get('_id.$oid');
+  //})
 });
--- app/models/user.js
+++ app/models/user.js
@@ -1,8 +1,13 @@
 import DS from 'ember-data';
 
 let User = DS.Model.extend({
+  _id: DS.attr(),
   email: DS.attr('string'),
   password: DS.attr('string'),
   password_confirmation: DS.attr('string')
+
+  //id: Ember.computed('_id', function(){
+  //  return this.get('_id.$oid');
+  //})
 });
 export default User;
