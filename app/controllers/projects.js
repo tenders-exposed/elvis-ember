1272cec02a5e2885672289da9caf0eb7e4aa01c7
Victor Nițu <victor@thesponge.eu>
2016-03-20 08:31:11 +0200

    Accounts and other templates

---
+++ app/controllers/projects.js
@@ -0,0 +1,6 @@
+import Ember from 'ember';
+import AuthenticatedRouteMixin from 'ember-simple-auth/mixins/authenticated-route-mixin';
+
+export default Ember.Controller.extend(AuthenticatedRouteMixin, {
+  session: Ember.inject.service('session'),
+});
