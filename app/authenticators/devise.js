045f4c192d03677c238a7024be48147516fbddd7
Victor Nițu <victor@thesponge.eu>
2016-05-01 21:48:32 +0300

    Query refactoring #1

--- app/authenticators/devise.js
+++ app/authenticators/devise.js
@@ -2,5 +2,5 @@ import DeviseAuthenticator from 'ember-simple-auth/authenticators/devise';
 import ENV from '../config/environment';
 
 export default DeviseAuthenticator.extend({
-  serverTokenEndpoint: `${ENV.APP.apiHost}/api/${ENV.APP.apiNamespace}/users/sign_in'`
+  serverTokenEndpoint: `${ENV.APP.apiHost}/api/${ENV.APP.apiNamespace}/users/sign_in`
 });
