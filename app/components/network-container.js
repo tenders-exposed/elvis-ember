c6171d8353d882225f2f5a2aa153490b6f15775e
Victor Nițu <victor@thesponge.eu>
2016-06-18 23:09:24 +0300

    Vis.js bindings for network stabilization progress

--- app/components/network-container.js
+++ app/components/network-container.js
@@ -2,4 +2,12 @@ import Ember from 'ember';
 
 export default Ember.Component.extend({
   // tagName: 'div',
+  actions: {
+    startStabilizing() {
+      this.sendAction('startStabilizing');
+    },
+    stabilizationIterationsDone() {
+      alert('stabilization iterations done');
+    }
+  }
 });
