7557bb5da222ae8fd257953b18e14f306470eb30
Ioana <ioana@serenitymedia.ro>
2016-10-27 22:29:40 +0300

    account settings ajax request

--- app/models/user.js
+++ app/models/user.js
@@ -3,6 +3,8 @@ import DS from 'ember-data';
 
 const User = DS.Model.extend({
   email: DS.attr('string'),
+  name: DS.attr('string'),
+  country: DS.attr('string'),
   authentication_token: DS.attr('string'),
   password: DS.attr('string'),
   password_confirmation: DS.attr('string'),
--- app/routes/account/settings.js
+++ app/routes/account/settings.js
@@ -1,6 +1,25 @@
 import Ember from 'ember';
 
+
 export default Ember.Route.extend({
+  ajax: Ember.inject.service(),
+
+  actions: {
+    settings(model){
+      console.log("model",model);
+    },
+    deviseSendReset(password, password_confirmation){
+      let email= this.controller.get('model.email');
+      let data=  {"user": { "email": email, "password": password,"password_confirmation": password_confirmation}};
+      this.get('ajax').post('/users/password', {data: data,headers: { 'Content-Type': 'application/json' } })
+        .then((response)=>{
+           console.log("Response: ",response);
+        }, (response)=>{
+          console.log("Response: ",response);
+        });
+
+    }
+  },
 
   activate() {
     this.notifications.clearAll();
