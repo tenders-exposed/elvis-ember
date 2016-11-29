06ddf3bdd4f09fda9aa6efffa135dba2f99a538d
Ioana <ioana@serenitymedia.ro>
2016-11-29 12:34:42 +0200

    styling sidebar table

--- app/components/elvis-data-table.js
+++ app/components/elvis-data-table.js
@@ -1,11 +1,26 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+  //search word
   search: "",
+  //field Name to search
   searchField: "",
+  //label for search input placeholder
   searchLabel: "find...",
-  defaultContent: {no: 'content'},
+  //the content to revert to after a search was performed
+  defaultContent: {},
+  //table content
+  content: {},
 
+  //the action to send to the controller after a click on a table-row
+  action: "",
+  tableContent: {},
+  //columns for the table = {fieldName: "LabelName", ....}
+  fields: {},
+  //class for data-table element
+  class: "",
+
+  //reset all variables regarding search if search input has no value
   contentObserver: Ember.observer('search', function () {
     console.log('search = ', this.get('search'));
     if(!this.get('search')) {
@@ -14,13 +29,14 @@ export default Ember.Component.extend({
       this.set('content', this.get('defaultContent'));
     }
   }),
-
+  // sort - a property passed from data-table
   sortTable: Ember.observer('sort',function () {
     let order = _.startsWith(this.get('sort'), '-') ? 'asc' : 'desc';
-    let field = _.trimStart(this.get('sort'), '_-');
+    let field = _.trimStart(this.get('sort'), '-');
+    console.log('field ',field);
+
     let ordered = _.orderBy(this.get('content'),field, order);
     this.set('content', ordered);
-
   }),
 
   didInsertElement(){
@@ -33,19 +49,25 @@ export default Ember.Component.extend({
       this.sendAction('action', selection);
     },
 
+    //set the search field/label for search input
     searchColumn(label, field){
       this.set('searchField', field);
       this.set('searchLabel', `find ${label}...`);
     },
 
+    /*
+    search table
+    - everything transformed in lowercase
+    - if/else a column was set to search into
+    - reset the content of the table with the new searched one
+     */
     searchTable(){
       const searchField = this.get('searchField');
       const searchWord = _.toLower(_.toString(this.get('search')));
-      let self = this;
+      const self = this;
 
       let content = _.filter(this.get('defaultContent'), function (o) {
         let match = false;
-        //if no column has been selected search all columns
         if(!searchField) {
           _.forEach(self.get('fields'), function(value, fieldName) {
             const fieldValue = _.toLower(_.toString(o[fieldName]));
