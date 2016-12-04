0180fa7f0450df4cae338048ce27d63a0c706143
Ioana <ioana@serenitymedia.ro>
2016-12-04 14:29:42 +0200

    format the value column to be human readable
    Fixes #96

--- app/routes/network/show/details.js
+++ app/routes/network/show/details.js
@@ -5,6 +5,12 @@ export default Ember.Route.extend({
 
   setupController(controller) {
 
+    let valueFormat = (value) => {
+      value = value.toFixed();
+      value = value.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+      return value;
+    };
+
     let active = this.paramsFor('network.show.details.detail').type;
     if(active){
      controller.set('active', active);
@@ -18,11 +24,21 @@ export default Ember.Route.extend({
 
     controller.set(
       'gridOptions.suppliers.rowData',
-      _.filter(graphNodes, (o) => { return o.type === 'supplier' && o; })
+        _.filter(graphNodes, (o) => {
+          if(o.type === 'supplier'){
+            o.value = valueFormat(o.value);
+            return o;
+          }
+        })
     );
     controller.set(
       'gridOptions.procurers.rowData',
-      _.filter(graphNodes, (o) => {return o.type === 'procuring_entity' && o;})
+      _.filter(graphNodes, (o) => {
+        if(o.type === 'procuring_entity'){
+          o.value = valueFormat(o.value);
+          return o;
+        }
+      })
     );
 
     let relationships = this.controllerFor('network.show').get('model.graph.edges');
@@ -33,6 +49,7 @@ export default Ember.Route.extend({
       const toObj =  _.find(graphNodes, function(o) { return o.id === idTo; });
       value['fromLabel'] = fromObj.label;
       value['toLabel'] = toObj.label;
+      value['value'] = valueFormat(value.value);
     });
 
     controller.set('gridOptions.relationships.rowData', relationships);
