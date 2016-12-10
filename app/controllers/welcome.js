4e84f11b87235d05c545eca0c67b0a17ab7ed691
Victor Nițu <victor@thesponge.eu>
2016-12-10 20:02:00 +0200

    Fix REST adapter to handle non JSONAPI errors \o/

--- app/controllers/welcome.js
+++ app/controllers/welcome.js
@@ -9,42 +9,28 @@ export default Ember.Controller.extend({
   actions: {
     register() {
       let self = this;
-      return this.store.createRecord('user', {
+      let user = this.store.createRecord('user', {
         email: self.get('fakeUser.email'),
         password: self.get('fakeUser.password'),
         password_confirmation: self.get('fakeUser.password_confirmation')
-      }).save().then(function(response) {
-          self.notifications.clearAll();
-          self.notifications.success('Done! Please check your inbox.', {
-            autoClear: true
-          });
-          console.log('Response: ', response);
-          self.transitionToRoute('welcome');
-        },
-        function(response) {
-          self.notifications.clearAll();
-          _.forEach(response.errors, (error, index) => {
-            error.forEach((v) => {
-              self.notifications.error(`Error: ${index } ${v}`);
-            });
-            // JSON API version:
-            // self.notifications.error(`Error: ${error.source.pointer.replace('/data/attributes/', '')} ${error.detail}`);
-          });
+      });
 
-          // Disabled for now, as we don't have JSON API error responses yet
-
-          // console.error('There was a problem', response);
-          // Object.keys(response.errors).map(function(value, index) {
-          //   console.log(index);
-          //   response.errors.each(function() {
-          //     self.notifications.addNotification({
-          //       message: this[0],
-          //       type: 'error',
-          //       autoClear: true,
-          //       clearDuration: 2500
-          //     });
-          //   });
-          // });
+      return user.save().then(function(response) {
+        self.notifications.clearAll();
+        self.notifications.success('Done! Please check your inbox.', {
+          autoClear: true
+        });
+        console.log('Response: ', response);
+        self.transitionToRoute('welcome');
+      },
+      function(response) {
+        console.log(response);
+        self.notifications.clearAll();
+        _.forEach(response.errors, (error, index) => {
+          error.forEach((v) => {
+            self.notifications.error(`Error: ${index } ${v}!`);
+          });
+        });
         }
       );
     },
