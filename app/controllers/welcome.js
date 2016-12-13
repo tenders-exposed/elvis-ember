a57924cdd66ecb4accce675da725d16ab261feb7
Ioana <ioana@serenitymedia.ro>
2016-12-13 03:06:30 +0200

    rename networks
     Fixes #72

--- app/controllers/welcome.js
+++ app/controllers/welcome.js
@@ -9,11 +9,12 @@ export default Ember.Controller.extend({
   actions: {
     register() {
       let self = this;
-      return this.store.createRecord('user', {
+      let user =  this.store.createRecord('user', {
         email: self.get('fakeUser.email'),
         password: self.get('fakeUser.password'),
         password_confirmation: self.get('fakeUser.password_confirmation')
-      }).save().then(function(response) {
+      });
+      user.save().then(function(response) {
           self.notifications.clearAll();
           self.notifications.success('Done! Please check your inbox.', {
             autoClear: true
@@ -22,29 +23,13 @@ export default Ember.Controller.extend({
           self.transitionToRoute('welcome');
         },
         function(response) {
+          console.log("errors");
+          console.log(user.get('errors')); // instance of DS.Errors
+          console.log(user.get('errors').toArray());
+          console.log(user.get('isValid')); // false
+
+          console.log("erori-register", response);
           self.notifications.clearAll();
-          _.forEach(response.errors, (error, index) => {
-            error.forEach((v) => {
-              self.notifications.error(`Error: ${index } ${v}`);
-            });
-            // JSON API version:
-            // self.notifications.error(`Error: ${error.source.pointer.replace('/data/attributes/', '')} ${error.detail}`);
-          });
-
-          // Disabled for now, as we don't have JSON API error responses yet
-
-          // console.error('There was a problem', response);
-          // Object.keys(response.errors).map(function(value, index) {
-          //   console.log(index);
-          //   response.errors.each(function() {
-          //     self.notifications.addNotification({
-          //       message: this[0],
-          //       type: 'error',
-          //       autoClear: true,
-          //       clearDuration: 2500
-          //     });
-          //   });
-          // });
         }
       );
     },
