0894c5e7e2174c5154dcbdd8b3c7d46b553882a1
Ioana <ioana@serenitymedia.ro>
2016-12-13 03:06:30 +0200

    rename networks
     Fixes #72

--- app/controllers/network/show/details.js
+++ app/controllers/network/show/details.js
@@ -1,6 +1,10 @@
 import Ember from 'ember';
 
 export default Ember.Controller.extend({
+  me: Ember.inject.service(),
+  ajax: Ember.inject.service(),
+
+  networkInput: false,
   fields: {
     "suppliers": { id: "ID" , label: "Name", flags: " ", value: "Value"},
     "relationships": { fromLabel: "Procurer", toLabel: "Supplier", flags: " ", value: "Value"}
@@ -148,7 +152,53 @@ export default Ember.Controller.extend({
         animation: true
       });
       this.network.network.selectEdges([selection.id]);
-    }
+    },
+    toggleInput(){
+      this.toggleProperty('networkInput');
+      return false;
+    },
+    saveNetworkName(){
+      let networkName = this.get('model.name');
+      let networkId = this.get('model.id');
+      let token = this.get("me.data.authentication_token");
+      let email = this.get("me.data.email");
 
+      let data = `{"network": { "name": "${networkName}"}}`;
+      const headers = `{
+          "Content-Type": "application/json",
+          "X-User-Email': "${email}",
+          "X-User-Token': "${token}"
+      }`;
+
+      console.log("data = "+data);
+      console.log("headers = "+headers);
+      let self = this;
+
+      this.get('ajax')
+        .put(`/networks/${networkId}`, {
+          data,
+          headers: {
+            'Content-Type': 'application/json',
+            'X-User-Email': `${email}`,
+            'X-User-Token': `${token}`
+          }
+        }).then(
+          (response)=>{
+            console.log("response ", response);
+            if (this.get('session.isAuthenticated')) {
+              self.get('notifications').clearAll();
+              self.get('notifications').success('Done!', {autoClear: true});
+              this.send('toggleInput');
+            } else {
+              self.get('notifications').error(`Error: Please login to save your network!`);
+            }
+          },(response) => {
+            self.get('notifications').clearAll();
+            console.log(response);
+             _.forEach(response.errors, (error, index) => {
+               self.get('notifications').error(`Error: ${index } ${error.title}`);
+             });
+          });
+    }
   }
 });
