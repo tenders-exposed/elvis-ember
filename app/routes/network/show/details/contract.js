e03cc88f471b786de528d36c739017851f4dbfd2
Ioana <ioana@serenitymedia.ro>
2017-03-07 00:30:20 +0200

    work in progress2

--- app/routes/network/show/details/contract.js
+++ app/routes/network/show/details/contract.js
@@ -7,14 +7,20 @@ export default Route.extend({
   store: inject.service(),
 
   model(params){
+    //str.split(" ");
+    let paramsArr = params.id.split("-");
+    let contractId = paramsArr[0];
+    let entityId = paramsArr[1];
+    console.log("supplier", entityId);
+
     let self = this;
     let token     = this.get('me.data.authentication_token');
     let email     = this.get('me.data.email');
 
-    console.log(`params-id ${params.id}`);
+    console.log(`params-id ${contractId}`);
 
     return this.get('ajax')
-      .request(`/contracts/${params.id}`, {
+      .request(`/contracts/${contractId}`, {
         headers: {
           'Content-Type': 'application/json',
           'X-User-Email': `${email}`,
@@ -22,8 +28,39 @@ export default Route.extend({
         }
       }).then(
         (data) => {
-          console.log("results", data.search.results[0]);
-         return data.search.results[0];
+          let results = data.search.results[0];
+          results.supplier = results.suppliers[0];
+          results.entityId = entityId;
+
+          let details = {"yes" : "check", "no" : "close", "null" : "question"};
+          let dateHash = {
+            "1": "Jan",
+            "2": "Feb",
+            "3": "Mar",
+            "4": "Apr",
+            "5": "May",
+            "6": "Jun",
+            "7": "Jul",
+            "8": "Aug",
+            "9": "Sep",
+            "10": "Oct",
+            "11": "Nov",
+            "12": "Dec"
+          };
+
+          results.award.date.x_month = dateHash[results.award.date.x_month];
+          console.log("month", results.award.date.x_month);
+          results.x_eu_project = String(results.x_eu_project);
+          results.x_framework = String(results.x_framework);
+          results.x_subcontracted = String(results.x_subcontracted);
+
+
+          results.eu_project = details[results.x_project] ? details[results.x_project] : "check" ;
+          results.framework = details[results.x_framework]  ? details[results.x_framework] : "check" ;
+          results.subcontracted = details[results.x_subcontracted] ? details[results.x_subcontracted] : "check" ;
+
+          console.log("results", results);
+          return results;
         }, (response) => {
 
           self.get('notifications').clearAll();
@@ -31,6 +68,13 @@ export default Route.extend({
             self.get('notifications').error(`Error: ${index } ${error.title}`);
           });
         });
-
+  },
+  actions: {
+    closeDetails() {
+      this.transitionTo(
+        'network.show.details',
+        this.controllerFor('network.show.details').get('activeTab')
+      );
+    }
   }
 });
