c43b6f997dddeccb37b9767c426f0c29af8325e9
Ioana <ioana@serenitymedia.ro>
2017-03-12 22:41:32 +0200

    fix coding styles

--- app/routes/network/show/details/contract.js
+++ app/routes/network/show/details/contract.js
@@ -1,4 +1,5 @@
 import Ember from 'ember';
+import moment from 'moment';
 const { Route, inject } = Ember;
 
 export default Route.extend({
@@ -26,31 +27,17 @@ export default Route.extend({
           results.supplier = results.suppliers[0];
           results.entityId = entityId;
 
-          let details = {"yes" : "check", "no" : "close", "null" : "question"};
-          let dateHash = {
-            "1": "Jan",
-            "2": "Feb",
-            "3": "Mar",
-            "4": "Apr",
-            "5": "May",
-            "6": "Jun",
-            "7": "Jul",
-            "8": "Aug",
-            "9": "Sep",
-            "10": "Oct",
-            "11": "Nov",
-            "12": "Dec"
-          };
+          let details = {'yes' : 'check', 'no' : 'close', 'null' : 'question'};
 
-          results.award.date.x_month = dateHash[results.award.date.x_month];
+          results.award.date.full = `${results.award.date.x_year}-${results.award.date.x_month}-${results.award.date.x_day}`;
           results.x_eu_project = String(results.x_eu_project);
           results.x_framework = String(results.x_framework);
           results.x_subcontracted = String(results.x_subcontracted);
 
 
-          results.eu_project = details[results.x_project] ? details[results.x_project] : "check" ;
-          results.framework = details[results.x_framework]  ? details[results.x_framework] : "check" ;
-          results.subcontracted = details[results.x_subcontracted] ? details[results.x_subcontracted] : "check" ;
+          results.eu_project = details[results.x_project] ? details[results.x_project] : 'check' ;
+          results.framework = details[results.x_framework]  ? details[results.x_framework] : 'check' ;
+          results.subcontracted = details[results.x_subcontracted] ? details[results.x_subcontracted] : 'check' ;
 
           return results;
         }, (response) => {
@@ -61,6 +48,7 @@ export default Route.extend({
           });
         });
   },
+
   actions: {
     closeDetails() {
       this.transitionTo(
