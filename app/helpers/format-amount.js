d968eb1ddbef4c968fca13725f062e2e95a7247a
Ioana <ioana@serenitymedia.ro>
2017-03-12 22:52:42 +0200

    format amounts on contract page

--- app/helpers/format-amount.js
+++ app/helpers/format-amount.js
@@ -5,7 +5,7 @@ const { Helper } = Ember;
 
 export function formatAmount(params/*, hash*/) {
   let [value] = params,
-      format = params[1] || '0.00 a';
+      format = params[1] || '0,0[.]00 a';
   return numeral(value).format(format).toUpperCase();
 }
 
