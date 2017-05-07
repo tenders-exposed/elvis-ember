f3a56bd31e0526ef8886c7f071f4ed6c3d479065
Victor Nițu <victor@thesponge.eu>
2017-05-08 00:16:56 +0300

    Clean up
    * `jsTree` assets that we weren't using
    * `scripts` folder, which was an early deployment artifact
    * Floobits configuration
    * an old mind map file

--- scripts/deploy.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env sh
-
-ssh -i config/id_rsa vu2003@strix.umbra.xyz mv elvis/htdocs elvis/htdocs-$(date +%s)
-ssh -i config/id_rsa vu2003@strix.umbra.xyz mkdir elvis/htdocs
-rsync -a -e 'ssh -i config/id_rsa' dist/* vu2003@strix.umbra.xyz:/var/www/virtual/tenders.exposed/elvis/htdocs/
