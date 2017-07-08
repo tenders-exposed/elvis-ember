8eb7a60d0e95bd21ce2bc88d45164829eb4d859d
Victor Nițu <victor@thesponge.eu>
2017-07-08 13:09:51 +0300

    Remove deprecated tests
    Some tests remained as generated from previously existing routes and
    controllers, while some were just attempting to test temporarily untestable
    things (e.g. components that need mock values to render properly).
    
    Related to #258

--- tests/unit/views/network/query-test.js
+++
@@ -1,9 +0,0 @@
-import { moduleFor, test } from 'ember-qunit';
-
-moduleFor('view:network/query', 'Unit | View | network/query');
-
-// Replace this with your real tests.
-test('it exists', function(assert) {
-  let view = this.subject();
-  assert.ok(view);
-});
