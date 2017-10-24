ecee062ed6079d31cb1e069f32e4e2033ead8301
Victor Nițu <victor@thesponge.eu>
2017-10-24 14:36:09 +0300

    Upgrade to Ember CLI 2.16.2 (#344)
    * Upgrade to Ember CLI 2.16.2
    
    Also begin the transition to a new import standard
    
    * Pass linting

--- app/router.js
+++ app/router.js
@@ -1,9 +1,7 @@
-import Ember from 'ember';
+import EmberRouter from '@ember/routing/router';
 import config from './config/environment';
 
-const { Router } = Ember;
-
-const ElvisRouter = Router.extend({
+const ElvisRouter = EmberRouter.extend({
   location: config.locationType,
   rootURL: config.rootURL
 });
