7ceea821817f646a6b5027119cf8c685befc296f
Victor Nițu <victor@thesponge.eu>
2017-11-16 16:06:28 +0100

    Refactor login form and modal (#362)
    This puts the login in a component and destroys the awkward routing around it.
    
    * new `login-form` component
    * modal and action on `elvis-nav` instead of app controllers
    * new route for `account/login` that contains the same form.

---
+++ app/routes/account/login.js
@@ -0,0 +1,10 @@
+import Route from '@ember/routing/route';
+
+export default Route.extend({
+
+  activate() {
+    if (this.get('session.isAuthenticated')) {
+      this.transitionTo('welcome');
+    }
+  }
+});
