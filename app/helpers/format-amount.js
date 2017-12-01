2c6e711c1f95a92c267a1be2696dd3e11f8f8c1e
Victor Nițu <victor@thesponge.eu>
2017-12-01 17:46:43 +0200

    Replace values of 0 with "no data"

--- app/helpers/format-amount.js
+++ app/helpers/format-amount.js
@@ -7,12 +7,12 @@ const defaultFormat = '0,0[.]00 a';
 export function formatAmount(params/*, hash*/) {
   let [value, format] = params;
   let type = typeof value;
-  if (type !== 'undefined' && type !== 'NaN' && type !== 'object') {
+  if (type !== 'undefined' && type !== 'NaN' && type !== 'object' && type > 0) {
     return numeral(value)
       .format(format || defaultFormat)
       .toUpperCase();
   } else {
-    return 'n/a';
+    return 'no data';
   }
 }
 
