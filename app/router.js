b88e88bc9baa60107d3788f3502dc3d5c5614fdb
Victor Nițu <victor@thesponge.eu>
2017-12-01 12:08:57 +0200

    [#137] Add Piwik tracking (#373)
    Fixes #137

--- app/router.js
+++ app/router.js
@@ -1,7 +1,8 @@
 import EmberRouter from '@ember/routing/router';
 import config from './config/environment';
+import Piwik from 'ember-cli-piwik/mixins/page-view-tracker';
 
-const ElvisRouter = EmberRouter.extend({
+const ElvisRouter = EmberRouter.extend(Piwik, {
   location: config.locationType,
   rootURL: config.rootURL
 });
