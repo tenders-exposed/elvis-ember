0d3c7e0f4fd95c2c669a6a0b079ec826f006097b
Victor Nițu <victor@thesponge.eu>
2017-12-11 21:00:24 +0200

    Tweak placeholders

--- app/helpers/format-amount.js
+++ app/helpers/format-amount.js
@@ -5,14 +5,17 @@ const { Helper } = Ember;
 const defaultFormat = '0,0[.]00 a';
 
 export function formatAmount(params/*, hash*/) {
-  let [value, format] = params;
+  let [value, format, placeholder] = params;
   let type = typeof value;
   if (type !== 'undefined' && type !== 'NaN' && type !== 'object' && type > 0) {
     return numeral(value)
       .format(format || defaultFormat)
       .toUpperCase();
   } else {
-    return 'no data';
+    if (placeholder) {
+      return placeholder;
+    }
+    return undefined;
   }
 }
 
