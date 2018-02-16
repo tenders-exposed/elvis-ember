c9ecf996edac66be9997fccd353419a8e7f07235
Victor Nițu <victor@thesponge.eu>
2018-02-16 16:14:53 +0200

    Add logged in user details

--- app/services/me.js
+++ app/services/me.js
@@ -1,9 +1,29 @@
 import Service from '@ember/service';
 import { computed } from '@ember/object';
 import { inject as service } from '@ember/service';
+import ENV from '../config/environment';
+import RSVP from 'rsvp';
 
 export default Service.extend({
   session: service(),
+  ajax: service(),
+  endpoint: `${ENV.APP.apiHost}/account/login`,
+
+  details: computed('data', function() {
+    let options = {
+      headers: {
+        'Authorization': this.get('data').access_token
+      },
+      method: 'GET'
+    };
+
+    let self = this;
+    return new RSVP.Promise((resolve, reject) => {
+      fetch(this.get('endpoint'), options).then((response) => {
+        return self.set('user', response);
+      }).catch(reject);
+    });
+  }),
 
   init() {
     let routeName = window.location.href.split('/').pop();
@@ -15,7 +35,11 @@ export default Service.extend({
 
     this.set('data', computed('session.session.content.authenticated', function() {
       // console.log('session', this.get('session.session.content.authenticated'));
-      return this.get('session.session.content.authenticated');
+      let data = this.get('session.session.content.authenticated');
+      return data;
     }));
+
+
+
   }
 });
