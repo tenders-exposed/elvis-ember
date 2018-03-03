b52731b566e1cf10d956acb08ef725b60951b2b3
Victor Nițu <victor@thesponge.eu>
2018-03-03 15:25:49 +0200

    Remove user's unwanted attributes in serializer

--- app/serializers/user.js
+++ app/serializers/user.js
@@ -3,27 +3,20 @@ import DS from 'ember-data';
 const { JSONSerializer } = DS;
 
 export default JSONSerializer.extend({
-  // primaryKey: 'Authorization'
-  //   serialize() {
-  //     let json = this._super(...arguments);
-  //
-  //     let attributes = [
-  //       'id',
-  //       'email',
-  //       'password',
-  //       'token',
-  //       'password_confirmation',
-  //       'country',
-  //       'name'
-  //     ];
-  //
-  //     json.user = {};
-  //
-  //     attributes.forEach((v) => {
-  //       json.user[v] = json[v];
-  //       delete json[v];
-  //     });
-  //
-  //     return json;
-  //   }
+  serialize() {
+    let json = this._super(...arguments);
+
+    let attributes = [
+      'email',
+      'password'
+    ];
+
+    Object.keys(json).forEach((k) => {
+      if (!attributes.includes(k)){
+        delete json[k];
+      }
+    });
+
+    return json;
+  }
 });
