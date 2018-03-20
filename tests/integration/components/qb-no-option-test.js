d62e11a37024c43224c658ed91e86cc207be7d14
Victor Nițu <victor@thesponge.eu>
2018-03-20 13:48:47 +0200

    Add a loading spinner for query builder options

---
+++ tests/integration/components/qb-no-option-test.js
@@ -0,0 +1,24 @@
+import { moduleForComponent, test } from 'ember-qunit';
+import hbs from 'htmlbars-inline-precompile';
+
+moduleForComponent('qb-no-option', 'Integration | Component | qb no option', {
+  integration: true
+});
+
+test('it renders', function(assert) {
+  // Set any properties with this.set('myProperty', 'value');
+  // Handle any actions with this.on('myAction', function(val) { ... });
+
+  this.render(hbs`{{qb-no-option}}`);
+
+  assert.equal(this.$().text().trim(), '');
+
+  // Template block usage:
+  this.render(hbs`
+    {{#qb-no-option}}
+      template block text
+    {{/qb-no-option}}
+  `);
+
+  assert.equal(this.$().text().trim(), 'template block text');
+});
