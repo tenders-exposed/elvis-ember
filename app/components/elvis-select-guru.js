077ddc2c2c35626ce0036845d9f71b5c238fc60a
Ioana <ioana@serenitymedia.ro>
2018-05-25 17:19:29 +0300

    WIP actor selector scrolling and selecting

---
+++ app/components/elvis-select-guru.js
@@ -0,0 +1,42 @@
+import EmberSelectGuru from 'ember-select-guru/components/ember-select-guru';
+const {  get, run } = Ember;
+
+
+export default EmberSelectGuru.extend({
+
+  actions: {
+    onOptionCheck(option) {
+      console.log('onOptionCheck');
+    },
+    onOptionClick(option) {
+      if(this.get('multiple')) {
+        // handle multiple selection
+        console.log('onOptionClick multiple option', option);
+        console.log('onOptionClick multiple _value', this.get('_value'));
+
+        this.get('_value').pushObject(option);
+        this.attrs.onSelect(this.get('_value'));
+      } else {
+        // handle single selection
+        this.attrs.onSelect(option);
+      }
+    },
+    onRemoveValueClick(option) {
+      this.get('_value').removeObject(option);
+      this.attrs.onSelect(this.get('_value'));
+    },
+    expandComponent() {
+      if(!this.get('isExpanded')) { this._resetCurrentHighlight(); }
+      this.set('isExpanded', true);
+      // this.set('queryTerm', 'ibm');
+      // console.log('extended function');
+      run.schedule('afterRender', this, function() {
+        this.$('input').focus();
+      });
+    },
+    willHideDropdown() {
+      // console.log('willHideDropdown');
+      this.set('isExpanded', false);
+    }
+  }
+});
